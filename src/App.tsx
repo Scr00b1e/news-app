e60f68e44e5d7573c4cb6c33aa4843d31e46688e
Jack <kbolot312@gmail.com>
2023-03-11 22:35:23 +0600

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-
+import Header from "./components/Header";
 
 function App() {
 
   return (
-    <div className="text-white bg-slate-600">
-      <p className='text-white text-6xl'>hello there!</p>
+    <div className="w-full relative">
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+    return (
+        <div className='w-full h-10 py-5 bg-stone-400 flex justify-center items-center fixed top-0 left-0'>
+            <p className='text-white text-2xl'>News</p>
+        </div>
+    )
+}
+
+export default Header
