29bc22bdc0793ea2cbd0c3e3671ff28c681cf115
Jack <kbolot312@gmail.com>
2023-03-13 22:04:42 +0600

    added pages item

---
+++ src/components/PagesItem.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const PagesItem = () => {
+    return (
+        <div className='w-full my-5 p-5 flex justify-center items-center flex-col bg-slate-200'>
+            <h1 className='text-2xl font-bold mb-3'>Some stuff is going on</h1>
+            <p className='text-xl opacity-70 max-w-xl mb-3'>Some stuff Some stuff Some stuff Some stuff Some stuff Some stuff
+                Some stuff Some stuff Some stuff Some stuff
+            </p>
+            <button className='px-3 py-1 border-gray-700 border-2 rounded-lg hover:bg-slate-700
+            hover:text-white duration-200'>Read more</button>
+        </div>
+    )
+}
+
+export default PagesItem
--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import PagesItem from '../components/PagesItem'
 
 const Pages = () => {
     return (
-        <div>
-            Pages
+        <div className='w-full'>
+            <h1 className='text-2xl font-bold text-center'>Welcome to Pages!</h1>
+            <PagesItem />
+            <PagesItem />
+            <PagesItem />
         </div>
     )
 }
