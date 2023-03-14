6dd683da17c1caac27bc373b61e680ffd72ef16c
Jack <kbolot312@gmail.com>
2023-03-14 20:28:14 +0600

    editing header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='w-full h-10 py-5 mt-3 bg-slate-500 flex justify-center items-center'>
-            Footer
+        <div className='w-full h-10 py-4 mt-3 bg-slate-500 flex justify-center items-center'>
+            <a href="https://github.com/Scr00b1e/news-app">
+                <img src="https://cdn-icons-png.flaticon.com/512/2111/2111432.png" alt=""
+                    className='w-7 h-7 cursor-pointer mx-1' />
+            </a>
+            <a href="https://twitter.com/scr00b1e">
+                <img src="https://cdn-icons-png.flaticon.com/512/3669/3669691.png" alt=""
+                    className='w-7 h-7 cursor-pointer mx-1' />
+            </a>
         </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom'
 
 const Header: React.FC = () => {
     return (
-        <div className='w-full h-10 py-5 bg-slate-500 flex justify-center items-center fixed top-0 left-0 z-20'>
+        <div className='w-full h-10 py-5 px-7 bg-slate-500 flex justify-between items-center fixed top-0 left-0 z-20'>
             <Link to={'/'}>
-                <p className='text-white text-2xl'>News</p>
+                <p className='text-white text-3xl'>News</p>
             </Link>
+            <button className='py-1 px-3 bg-orange-400 text-slate-50'>Login</button>
         </div>
     )
 }
