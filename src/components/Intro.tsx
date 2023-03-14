57cc964fb3473dab009b8d9a3c18069cbb7bb3e0
Jack <kbolot312@gmail.com>
2023-03-14 21:27:04 +0600

    big ui change

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,38 +1,33 @@
 import React from 'react'
+import { routings } from '../data/routings'
+
+type RoutingsType = {
+    img: any
+    text: string
+}
 
 const Intro = () => {
+    const Routings = ({ img, text }: RoutingsType) => {
+        return (
+            <div className='w-full h-20 my-5 bg-center bg-no-repeat bg-cover rounded-lg cursor-pointer'
+                style={img}>
+                <h1 className='mt-4 text-3xl font-bold text-center'>{text}</h1>
+            </div>
+        )
+    }
+
     return (
-        <div className='w-full py-3 px-5 bg-neutral-800 rounded-2xl'>
+        <div className='w-full py-3 px-5 bg-gray-800 rounded-2xl'>
             <div className='text-center text-slate-100'>
                 <h1 className='text-3xl'>Welcome to News!</h1>
                 <p className='text-xl'>Here you got</p>
             </div>
-            <div className='w-full flex items-center justify-center flex-col flex-wrap text-slate-200'>
-                <div className='relative'>
-                    <img src="https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg?w=2000" alt=""
-                        className='w-full h-25 rounded-lg my-5' />
-                    <h1 className='absolute top-12 left-16 text-3xl font-bold'>Sport</h1>
-                </div>
-                <div className='relative'>
-                    <img src="https://www.state.gov/wp-content/uploads/2020/11/shutterstock_186964970-scaled.jpg" alt=""
-                        className='w-full h-25 rounded-lg my-5' />
-                    <h1 className='absolute top-12 left-16 text-3xl font-bold'>Travel</h1>
-                </div>
-                <div className='relative'>
-                    <img src="https://psych.ubc.ca/wp-content/uploads/sites/2/2021/09/Religion_culture_Feature.png" alt=""
-                        className='w-full h-25 rounded-lg my-5' />
-                    <h1 className='absolute top-12 left-16 text-3xl font-bold'>Culture</h1>
-                </div>
-                <div className='relative'>
-                    <img src="https://cdn.cnn.com/cnnnext/dam/assets/180723132324-tomorrowland-future-city-super-169.jpg" alt=""
-                        className='w-full h-25 rounded-lg my-5' />
-                    <h1 className='absolute top-12 left-16 text-3xl font-bold'> Future</h1>
-                </div>
-                <div className='relative'>
-                    <img src="https://inkforall.com/wp-content/uploads/2022/12/6ca2b16d-2d9e-f7ae-97ec-89ad71b90197.jpg" alt=""
-                        className='w-full h-25 rounded-lg my-5' />
-                    <h1 className='absolute top-12 left-16 text-3xl font-bold'>Others</h1>
-                </div>
+            <div className='h-15 max-w-20 items-center flex justify-center flex-wrap text-slate-200'>
+                {
+                    routings.map((obj, i) => (
+                        <Routings {...obj} key={i} />
+                    ))
+                }
             </div>
         </div>
     )
---
+++ src/data/routings.ts
@@ -0,0 +1,22 @@
+export const routings = [
+    {
+        img: { backgroundImage: 'url(https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg?w=2000)' },
+        text: 'Sport'
+    },
+    {
+        img: { backgroundImage: 'url(https://www.state.gov/wp-content/uploads/2020/11/shutterstock_186964970-scaled.jpg)' },
+        text: 'Travel'
+    },
+    {
+        img: { backgroundImage: 'url(https://psych.ubc.ca/wp-content/uploads/sites/2/2021/09/Religion_culture_Feature.png)' },
+        text: 'Culture'
+    },
+    {
+        img: { backgroundImage: 'url(https://cdn.cnn.com/cnnnext/dam/assets/180723132324-tomorrowland-future-city-super-169.jpg)' },
+        text: 'Future'
+    },
+    {
+        img: { backgroundImage: 'url(https://inkforall.com/wp-content/uploads/2022/12/6ca2b16d-2d9e-f7ae-97ec-89ad71b90197.jpg)' },
+        text: 'Others'
+    }
+]
