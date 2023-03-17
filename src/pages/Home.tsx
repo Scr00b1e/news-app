cd58c66d7eb21978a079bbaa5da63b37a17aa8e9
Jack <kbolot312@gmail.com>
2023-03-17 13:28:25 +0600

    fetching data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,29 @@ import { Link } from 'react-router-dom'
 import Intro from '../components/Intro'
 import Routing from '../components/Routing'
 import { pages } from "../data/pages"
+import { query, onSnapshot, collection } from 'firebase/firestore'
+import { db } from '../firebaseconfig'
 
 const Home = () => {
+    const [somePages, setSomePages] = React.useState([])
+    const [loading, setLoading] = React.useState(true)
+
+    React.useEffect(() => {
+        const GetPages = async () => {
+            const q = query(collection(db, 'pages'))
+            const unsubscribe = onSnapshot(q, (querySnapshot) => {
+                let pages = []
+                querySnapshot.forEach((doc) => {
+                    pages.push({ ...doc.data(), id: doc.id })
+                })
+                setSomePages(pages)
+                setLoading(false)
+            })
+            return () => unsubscribe()
+        }
+        GetPages()
+    }, [])
+
     return (
         <>
             <Intro />
