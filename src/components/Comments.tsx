ba64b5e9d4b8957fe1b99d2a2473aa8c260c7e5c
Jack <kbolot312@gmail.com>
2023-03-21 22:10:49 +0600

    you can comment now

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,4 +1,4 @@
-import { collection, onSnapshot, query } from 'firebase/firestore'
+import { addDoc, collection, onSnapshot, query } from 'firebase/firestore'
 import React from 'react'
 import { db } from '../firebaseconfig'
 import CommentsItem from './CommentsItem'
@@ -6,7 +6,22 @@ import CommentsItem from './CommentsItem'
 const Comments = () => {
     const [comments, setComments] = React.useState([])
     const [loading, setLoading] = React.useState(false)
+    const [input, setInput] = React.useState('')
 
+    //add
+    const onAdd = async (e: any) => {
+        e.preventDefault()
+        if (input === '') {
+            alert('You havent wrote anything')
+            return
+        }
+        await addDoc(collection(db, 'comments'), {
+            text: input
+        })
+        setInput('')
+    }
+
+    //fetch
     React.useEffect(() => {
         try {
             const q = query(collection(db, 'comments'))
@@ -28,14 +43,17 @@ const Comments = () => {
         <div className='w-full'>
             <h1 className='text-xl font-bold'>Comments</h1>
             <input type="text" placeholder='Add a comment'
-                className='w-full border-b-2 border-black text-xl focus:outline-none' />
+                className='w-full border-b-2 border-black text-xl focus:outline-none'
+                value={input} onChange={(e) => setInput(e.target.value)} />
             <button className='px-3 text-xl font-bold uppercase bg-cyan-400 rounded-xl text-white
-            hover:bg-cyan-600 duration-200 mt-3'>Submit</button>
+            hover:bg-cyan-600 duration-200 mt-3' onClick={onAdd}>Submit</button>
             <div className='my-5'>
                 {
-                    comments.map((obj: any, i) => (
-                        <CommentsItem {...obj} key={i} />
-                    ))
+                    loading
+                        ? <div className='w-full text-center'><h1 className='text-xl'>Loading...</h1></div>
+                        : comments.map((obj: any, i) => (
+                            <CommentsItem {...obj} key={i} />
+                        ))
                 }
             </div>
         </div>
